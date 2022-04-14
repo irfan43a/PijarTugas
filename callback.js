dc2ce99407747a2e90b6bcaf3ce0f36e1f52523d
irfan43a <98889949+irfan43a@users.noreply.github.com>
2022-04-14 07:35:46 +0700

    Delete callback.js

--- callback.js
+++
@@ -1,8 +0,0 @@
-const penjumlahan = (value1, value2, cetak) => {
-  const result = value1 + value2;
-  cetak(result);
-};
-
-penjumlahan(10, 30, (hasil) => {
-  console.log(`hasil penjumlahan nya adalaha ${hasil}`);
-});
